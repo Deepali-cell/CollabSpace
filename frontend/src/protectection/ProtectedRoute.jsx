ea15109dbe197047b1f071e2b7a7a2810cbba7ea
Deepali <joysingal247@gmail.com>
2026-03-03 22:30:00 +0530

    minor issue in protected route

--- frontend/src/protectection/ProtectedRoute.jsx
+++ frontend/src/protectection/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
+import { StateContext } from "@/context/stateContext";
 import React, { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { stateContext } from "@/context/stateContext";
 
 const ProtectedRoute = () => {
-  const { user, accessToken } = useContext(stateContext);
+  const { user, accessToken } = useContext(StateContext);
 
   if (!user || !accessToken) {
     return <Navigate to="/authenticate" replace />;
